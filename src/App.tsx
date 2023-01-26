28333b2d10f39e8df00bc2035e855fe28f901937
django_freeman <o.tampodo.t.18@gmail.com>
2023-01-26 18:35:40 +0900

    wallet connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import { Button } from "@mui/material";
+
 function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const connectWallet = async() => {
+    const Window = window as any
+    if(!Window.ethereum) console.log("ethereum object not foud");
+    const accounts: string = await Window.ethereum.request({ method: "eth_requestAccounts"});
+    if (accounts.length === 0) console.log("account not found");
+    setCurrentAccount(accounts[0]);
+  }
+
+  const checkWalletConnection =async () => {
+    const Window = window as any;
+    if(!Window.ethereum) console.log("ethereum object not found");
+    const accounts: string = await Window.ethereum.request({ method: "eth_accounts"});
+    if (accounts.length === 0) console.log("accounts not found");
+    setCurrentAccount(accounts[0]);
+  }
+
+  useEffect(()=> {
+    checkWalletConnection();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant='contained' color='primary' onClick={connectWallet}>
+        connect wallet
+      </Button>
     </div>
   );
 }
