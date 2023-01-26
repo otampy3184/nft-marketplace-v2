b263722691d69ef91226f41a9f7c3dad69436f6c
django_freeman <o.tampodo.t.18@gmail.com>
2023-01-26 22:10:41 +0900

    コントラクトデプロイ

--- src/blockchain/hardhat.config.ts
+++ src/blockchain/hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: process.env.API_KEY || "",
+      accounts: process.env.SECRET_KEY !== undefined ? [process.env.SECRET_KEY] : [],
+    },
+    mumbai: {
+      url: process.env.API_KEY_MUM || "",
+      accounts: process.env.SECRET_KEY !== undefined ? [process.env.SECRET_KEY] : [],
+    },
+  }
 };
 
 export default config;
